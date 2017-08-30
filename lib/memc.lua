34982453c926ce953fa97dea16b92d07336abfc5
sqw <sqw@email.com>
2017-08-30 17:42:39 +0800

    bug

--- lib/memc.lua
+++ lib/memc.lua
@@ -1,57 +1,62 @@
-local memcached    = require "resty.memcached"
-local mc_config    = require "conf.memc"
-local cjson        = require "cjson"
-local say          = ngx.say
-local debug        = false
-local _M           = {}
+local memcached = require "resty.memcached"
+local mc_config = require "conf.memc"
+local cjson = require "cjson.safe"
+local say = ngx.say
+local os_date = os.date
+local table_insert = table.insert
+local ngx_ctx_bug = ngx.ctx.bug
+local type = type
+local _M = {}
 
-local function showError(...)
-    if debug then
-        say("failed to : ", ...)
+
+
+function _M:new()
+    local mem, err = memcached:new()
+    if not mem then
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .. " failed to instantiate memc: " ..  err)
+        return false
     end
+    mem:set_timeout(1000)
+    return mem
 end
-
-local memc, err = memcached:new()
-if not memc then
-    showError("failed to instantiate memc: ", err)
-    return false
-end
-memc:set_timeout(1000)
-memc:set_keepalive(10000,1024) 
-
 
 function _M:connect(memc_name)
+    local mem = self:new(memc_name)
     local host = mc_config[memc_name]["host"]
     local port = mc_config[memc_name]["port"]
-    local ok, err = memc:connect(host, port)
+    local ok, err = mem:connect(host, port)
     if not ok then
-        showError("failed to connect: "..host..":"..port, err)
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .. "failed to connect: " .. host .. ":" .. port .. " " .. err)
         return false
+    end
+    return mem
+end
+
+function _M:set_keepalive(mem)
+    local _, err = mem:set_keepalive(10000, 1000)
+    if err then
+        mem:close()
+
     end
 end
 
-function _M:get(key,memc_name)
-    self:connect(memc_name)    
-    local res, flags, err = memc:get(key)
+function _M:get(key, memc_name)
+    local mem = self:connect(memc_name)
+    local res, _, err = mem:get(key)
     if err then
-        showError("failed to get: ", err)
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .." failed to get: " .. err)
         return false
     end
-    if not res then
-        showError(key," not found")
-        return false
-    end
-    if type(res) == 'table' then
-        res = cjson.decode(res)
-    end
+    res = cjson.decode(res)
+    self:set_keepalive(mem)
     return res
 end
 
-function _M:set(key,val,memc_name,expire,flags)
+function _M:set(key, val, memc_name, expire, flags)
     if not val then
         return false
     end
-    self:connect(memc_name)    
+    local mem = self:connect(memc_name)
     if not expire then
         expire = 0
     end
@@ -61,21 +66,23 @@ function _M:set(key,val,memc_name,expire,flags)
     if type(val) == 'table' then
         val = cjson.encode(val)
     end
-    local ok, err = memc:set(key, val, expire, flags)
+    local ok, err = mem:set(key, val, expire, flags)
     if not ok then
-        showError("failed to set:",key, err)
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .. " failed to set: " .. key .. " "  ..err)
         return false
     end
+    self:set_keepalive(mem)
     return true
 end
 
-function _M:delete(key,memc_name)
-    self:connect(memc_name)
-    local ok, err = memc:delete(key, val)
+function _M:delete(key, memc_name)
+    local mem = self:connect(memc_name)
+    local ok, err = mem:delete(key)
     if not ok then
-        showError("failed to delete:",key, err)
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .. " failed to delete: ".. key .." " ..  err)
         return false
     end
+    self:set_keepalive(mem)
     return true
 end
 
--- lib/mysql.lua
+++ lib/mysql.lua
@@ -1,76 +1,81 @@
-local mysql         = require "resty.mysql"
-local util          = require "lib.util"
-local mysql_config  = require "conf.mysql"
-local say           = ngx.say
-local debug         = false
-local _M            = {}
+local mysql = require "resty.mysql"
+local strs = require "builder.string"
+local mysql_config = require "conf.mysql"
+local os_date = os.date
+local table_insert = table.insert
+local ngx_ctx_bug = ngx.ctx.bug
+local _M = {}
 
 
-local function showError(...)
-    if debug then
-        say("failed to : ", ...)
-    end
-end
 
 --实例化
-local db, err = mysql:new()
-if not db then
-    showError(err)
-    return false
+function _M:new()
+    local db, err = mysql:new()
+    if not db then
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .. " failed to instantiate mysql: ".. err)
+        return false
+    end
+    db:set_timeout(1000)
+    return db
 end
 
---连接
-
-function _M.connect(self,mysql_name)
-    local ok, err, errno, sqlstate = db:connect{
-        host = mysql_config[mysql_name]["host"],
-        port = mysql_config[mysql_name]["port"],
-        user = mysql_config[mysql_name]["user"],
-        database = mysql_config[mysql_name]['database'],
-        password = mysql_config[mysql_name]["password"],
-        max_packet_size = 1024 * 1024 
+function _M:connect(mysql_name)
+    local db = self:new()
+    local host = mysql_config[mysql_name]['master']["host"]
+    local port = mysql_config[mysql_name]['master']["port"]
+    local ok, err, _, sqlstate = db:connect {
+        host = host,
+        port = port,
+        user = mysql_config[mysql_name]['master']["user"],
+        database = mysql_config[mysql_name]['master']['database'],
+        password = mysql_config[mysql_name]['master']["password"],
+        max_packet_size = 1024 * 1024
     }
     if not ok then
-        showError(err)
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .." failed to connect: " ..  err .. " " .. host.. " ".. port .. " " .. sqlstate)
         return false
     end
+    return db
 end
 
 
-function _M.query(self,sql,mysql_name)
-    self:connect(mysql_name)
+
+function _M:query(sql, mysql_name)
+    local db = self:connect(mysql_name)
     local res, err, errno, sqlstate = db:query(sql)
     if not res then
-        showError("bad result: ", err, ": ", errno, ": ", sqlstate, ".")
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .." bad result: " ..  err .. ": " ..  errno .. ": " .. sqlstate)
         return false
     end
+    self:set_keepalive(db)
     return res
 end
 
-function _M:sql()
-     
 
-end
-
-function _M:select(sql,mysql_name)
-    self:connect(mysql_name)
+--查询所有和全部
+function _M:select(sql, mysql_name)
+    local db = self:connect(mysql_name)
     local res, err, errno, sqlstate = db:query(sql)
     if not res then
-        showError("bad result: ", err, ": ", errno, ": ", sqlstate, ".")
+        table_insert(ngx_ctx_bug,os_date("%Y-%m-%d %H:%M:%S") .. "bad result: " .. err ..": ".. errno.. ": ".. sqlstate)
         return false
     end
-   local data = {}
-   for i, row in ipairs(res) do 
-       data[i] = row 
-    end  
-return data
+    self:set_keepalive(db)
+    return res
 end
 
-function _M:sql(sql)
-    local mtype = util.explode(" ",sql)
-    if mtype[1] == 'select' then 
-        return self:select()
-    
+function _M:set_keepalive(db)
+    local _, err = db:set_keepalive(10000, 1000)
+    if err then
+        db:close()
+    end
+end
+
+--暂时只查全部
+function _M:sql(sql, mysql_name)
+    local mtype = strs.explode(" ", sql)
+    if mtype[1] == 'select' then
+        return self:select(sql, mysql_name)
     end
 end
 
